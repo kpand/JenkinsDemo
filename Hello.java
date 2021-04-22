cf8b4e6d762544339ff37864bdd2ae8ba1c1bdb9
Kshama Pandey <kpandey@itconvergence.com>
2021-04-22 16:29:17 +0000

    add Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
-public class MyFirstJavaProgram {
+public class Hello {
 
    /* This is my first java program.
     * This will print 'Hello World' as the output
+	* MyFirstJavaProgram
     */
 
    public static void main(String []args) {
